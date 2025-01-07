01513ed533f7e00b5abc1f09f58882ff4cbb860e
valery-kiselev <voler@inbox.ru>
2025-01-07 17:41:59 +0400

    Adminpanel

--- modul19/task1/admin.py
+++ modul19/task1/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_filter = ('size', 'cost')
+    list_display = ('title', 'cost', 'size')
+    search_fields = ('title',)
+    list_per_page = 20
+
+@admin.register(Buyer)
+class BuyerAdmin(admin.ModelAdmin):
+    list_filter = ('balance', 'age')
+    list_display = ('name', 'balance', 'age')
+    search_fields = ('name',)
+    list_per_page = 30
+    readonly_fields = ('balance',)
