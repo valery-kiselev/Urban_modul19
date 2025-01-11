915dfdff0babde5dfd531cd770e7bab2fe55ce3a
valery-kiselev <voler@inbox.ru>
2025-01-11 20:22:39 +0400

    Postgresql

--- modul19/modul19/settings.py
+++ modul19/modul19/settings.py
@@ -74,10 +74,21 @@ WSGI_APPLICATION = 'modul19.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "test",
+        "USER": "postgres",
+        "PASSWORD": "Password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
